e7c4aa6f46e3b51d793af391eaeb654e85675f32
paulhth <paul.horvath@student.upt.ro>
2024-12-27 17:35:50 +0200

    Refactor

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,9 @@
+package src.main;
+import java.util.*; // Example import for utility classes
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        
     }
 }
