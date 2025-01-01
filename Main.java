127646c9dcb5b33b1d6924aad95f193a73df21ee
paulhth <paul.horvath@student.upt.ro>
2025-01-01 19:12:32 +0200

    testing prints

--- Main.java
+++ Main.java
@@ -23,6 +23,11 @@ public class Main {
         
         //result = verifier.verifyProof(verifier.getX(), prover.getY(), prover);
         //System.out.println("Verification result: " + (result ? "Convinced" : "Not convinced"));
+        double medie = 0;
+        int success = 0;
+        int max = 0, maxoverall = 0;
+        int iteration = 0;
+        for(int t=0;t<100;t++){
         boolean result = false;
         int numberOfIterations = 1000,
             iterator = 0;
@@ -30,18 +35,26 @@ public class Main {
             System.out.println("=================ROUND: " + iterator);
             prover.generateR();
             verifier.generateE();
-            prover.generateY(verifier.getE(), true); //MODIFY THIS TO FALSE TO MAKE IT ILLEGITIMATE
+            prover.generateY(verifier.getE(), false); //MODIFY THIS TO FALSE TO MAKE IT ILLEGITIMATE
             verifier.generateX(prover.getR());
             result = verifier.verifyProof(verifier.getX(), prover.getY(), prover);
             System.out.println();
+            max++;
+            if(max > maxoverall) {maxoverall = max; iteration = iterator;}
             if(!result) break;
         }
-
+            
+        max = 0;
         if(result){
             System.out.println("Verification result: Convinced --- " + numberOfIterations + " iterations");
+            success ++ ;
         } else {
             System.out.println("Verification result: Not convinced --- " + iterator + " iterations");
         }
+        }
+        medie = (double)success/100;
+        System.out.println("=================\n======STATS======\n=================\n" + //
+                        "Success rate: " + medie + "\n" + "Iterations: " + 100 + "\nRounds per iteration: " + 1000 + "\nMax round reached while illegitimate: " + maxoverall + "\nChance of this happening: " + (double)maxoverall/1000 + "\n" + "Happened on iteration: " + iteration + "/1000");
     }
 }
 
